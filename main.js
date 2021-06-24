87a35873366e8537717e6617c3cdda216ba14ba8
Matheus Braynner <m.braynner@outlook.com>
2021-06-24 15:07:28 -0300

    Quarto dia de nlw

--- main.js
+++ main.js
@@ -21,13 +21,13 @@ for (const link of links) {
 const header = document.querySelector('#header')
 const navHeight = header.offsetHeight
 
-window.addEventListener('scroll', function () {
+function changeHeaderWhenScroll() {
   if (window.scrollY >= navHeight) {
     header.classList.add('scroll')
   } else {
     header.classList.remove('scroll')
   }
-})
+}
 
 /* Tentimonials carousel slider swiper */
 
@@ -54,7 +54,27 @@ scrollReveal.reveal(
   #about .image, #about .text,
   #services header, #services .card,
   #testimonials header, #testimonials .testimonials,
-  #contact .text, #contact .links
+  #contact .text, #contact .links,
+  footer .brand, footer .social
   `,
   { interval: 100 }
 )
+
+/* Button back to top */
+
+const backToTopButton = document.querySelector('.back-to-top')
+
+function backTotop() {
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+/* When scroll */
+
+window.addEventListener('scroll', function () {
+  changeHeaderWhenScroll()
+  backTotop()
+})
